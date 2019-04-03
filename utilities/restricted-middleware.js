3a760aba0fc463109933f81175b965784b94389a
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-03 17:35:55 -0400

    MVP complete for day1. /users route is restricted, checks for token before delivering users data.

--- utilities/restricted-middleware.js
+++ utilities/restricted-middleware.js
@@ -1,7 +1,18 @@
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
+
 module.exports = (req, res, next) => {
-  if (req.session && req.session.user) {
-    next();
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: 'You must be logged in to do that.' });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
   } else {
-    res.status(401).json({ message: 'You must be logged in to do that.' });
+    res.status(401).json({ message: 'No token provided.' });
   }
 };
